70b68a6f3dac2fe7a2302e35461f70b8a6051986
Kenichi K. <daJ3YG26@gmail.com>
2025-08-14 11:15:11 +0900

    Create main_v.js

---
+++ main_v.js
@@ -0,0 +1,34 @@
+async function getPointData(dom) {
+    const result = [];
+
+    for (const row of rows) {
+        const dateTag = row.querySelector(".list__one__date");
+        const point = row.querySelector(".list__one__contents--info");
+    
+        result.push([dateTag.innerText, point.innerText]);
+    }
+
+    return result;
+}
+
+// ダウンロードするファイル情報
+const fileName = 'pointDataV.csv';  // ダウンロードするファイル名
+const delimiter = ',';  // 区切り文字
+const header = ['日付', 'ポイント'];  // CSVヘッダー
+
+// 全てのページの情報を取得
+getPointData(document).then(dataArray => {
+    // CSVダウンロード
+    dataArray.unshift(header);
+    // "文字列"は引用符で囲う
+    const dataStr = dataArray.map(arr => arr.map(s => (typeof s === "string") ? `"${s}"` : s).join(delimiter)).join('\n');
+    // const dataStr = dataArray.map(arr => arr.map(s => (typeof s === "string") ? s.replaceAll("\n", "  ") : s).join(delimiter)).join('\n');  // 改行コードを含めない場合
+    const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+    const blob = new Blob([bom, dataStr], {type: "text/csv"});
+
+    const downloadTag = document.createElement('a');
+    downloadTag.download = fileName;
+    downloadTag.href = URL.createObjectURL(blob);
+    downloadTag.click();
+    URL.revokeObjectURL(downloadTag.href);
+})
