8bea9dcc6c3f0d7c067577835c7deea5ddc23f50
Kenichi K. <daJ3YG26@gmail.com>
2025-08-17 18:23:08 +0900

    Update main_d.js

--- main_d.js
+++ main_d.js
@@ -21,7 +21,7 @@ const fileName = 'pointDatad.csv';  // ダウンロードするファイル名
 const delimiter = ',';  // 区切り文字
 const header = ['日付'];  // CSVヘッダー
 dataArray = getPointData(document);
-alert(dataArray[0]);
+alert(dataArray);
 
     // CSVダウンロード
     dataArray.unshift(header);
@@ -30,7 +30,7 @@ alert(dataArray[0]);
     // const dataStr = dataArray.map(arr => arr.map(s => (typeof s === "string") ? s.replaceAll("\n", "  ") : s).join(delimiter)).join('\n');  // 改行コードを含めない場合
     const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
     const blob = new Blob([bom, dataStr], {type: "text/csv"});
-
+alert("create");
     const downloadTag = document.createElement('a');
     downloadTag.download = fileName;
     downloadTag.href = URL.createObjectURL(blob);
